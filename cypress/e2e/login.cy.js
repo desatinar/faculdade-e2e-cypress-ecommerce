ba0c1c186407e0d32cfaf13ce103ac0b91c17c89
Rodrigo Nascimento <rodrigo.nascimento@entrounaconta.com>
2026-05-27 09:44:48 -0300

    feat: acesso a página de login; preenche usuário válido; preenche senha inválida; clica em login; valida mensagem de erro.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -11,4 +11,12 @@ describe('Login de Usuário', () => {
         cy.contains('Swag Labs')
         cy.url().should('include', '/inventory')
     })
+
+    it('Deve fazer login com erro quando as credenciais foram inválidas', () => {
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret')
+        cy.get('#login-button').click()
+
+        cy.contains('Epic sadface: Username and password do not match any user in this service')
+    })
 })
